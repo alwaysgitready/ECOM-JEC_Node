b116d9df0d400867c32689aa954e3e2245a984a8
Tech Community <='alwaysreadygit@gmail.com>
2023-11-06 18:11:05 +0530

    initial Commit

---
+++ DB/DB.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/JEC-2023' , {useNewUrlParser  : true})
+
+
+const db =  mongoose.connection;
+
+db.on('error'  , ()=>{
+    console.log("Failed to Connect With MongoDB")
+})
+
+
+db.once('open' , ()=>{
+
+console.log('Successfully Connected With MongoDB')
+
+} )
+
+
+module.exports = db
+
+
